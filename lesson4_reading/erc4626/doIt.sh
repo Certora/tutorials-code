4189a10e14e60bedc7e001fe14ff7c3fea2fe1f1
Johannes Spaeth <johannes@certora.com>
2023-09-13 11:26:47 +0200

    Add info to bash script

--- lesson4_reading/erc4626/doIt.sh
+++ lesson4_reading/erc4626/doIt.sh
@@ -1,5 +1,9 @@
 for file in ./src/certora/conf/*.conf
+
+echo "Using git commit " >> results.out
+git rev-parse HEAD >> results.out
+git git status --short >> results.out
 do
-  echo -e "Results for $file can be found at:" >> results.out
+  echo  "Results for $file can be found at:" >> results.out
   certoraRun "$file" | sed -n 's/.*\(https:\/\/prover.certora.com\/output\/\)/\1/p' >> results.out
 done
