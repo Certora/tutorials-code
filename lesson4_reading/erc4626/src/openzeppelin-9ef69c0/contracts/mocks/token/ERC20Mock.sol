6c3008604f3dadc999e536dfaea4ac6e3363ec9f
Johannes Spaeth <johannes@certora.com>
2023-09-15 12:13:58 +0200

    Results for run:  with arguments src/certora/conf-openzeppelin/certoraRun-RoundingProps.conf

--- lesson4_reading/erc4626/src/openzeppelin-9ef69c0/contracts/mocks/token/ERC20Mock.sol
+++ lesson4_reading/erc4626/src/openzeppelin-9ef69c0/contracts/mocks/token/ERC20Mock.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC20} from "../../token/ERC20/ERC20.sol";
 
 contract ERC20Mock is ERC20 {
-    constructor() ERC20("ERC20Mock", "E20M") {}
+    constructor() {}
 
     function mint(address account, uint256 amount) external {
         _mint(account, amount);
