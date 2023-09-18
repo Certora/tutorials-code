9c37d73f332bc963ac194d588071d9b1f542a940
Johannes Spaeth <johannes@certora.com>
2023-09-18 09:49:58 +0200

    Simplifying withdraw logic (munging)

--- lesson4_reading/erc4626/src/openzeppelin-9ef69c0/contracts/token/ERC20/extensions/ERC4626.sol
+++ lesson4_reading/erc4626/src/openzeppelin-9ef69c0/contracts/token/ERC20/extensions/ERC4626.sol
@@ -275,7 +275,7 @@ contract ERC4626 is ERC20, IERC4626 {
         // Conclusion: we need to do the transfer after the burn so that any reentrancy would happen after the
         // shares are burned and after the assets are transferred, which is a valid state.
         _burn(owner, shares);
-        SafeERC20.safeTransfer(_asset, receiver, assets);
+        _asset.transfer(receiver, assets);
 
         emit Withdraw(caller, receiver, owner, assets, shares);
     }
